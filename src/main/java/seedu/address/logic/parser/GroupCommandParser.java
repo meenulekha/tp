f7e7f7cdf6afd6c8cab3c4190bce90c1eefd346e
julia-cp <juliac.purba@gmail.com>
2024-03-29 17:34:03 +0800

    add comments

--- src/main/java/seedu/address/logic/parser/GroupCommandParser.java
+++ src/main/java/seedu/address/logic/parser/GroupCommandParser.java
@@ -4,13 +4,19 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.commands.GroupCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+/**
+ * Parses input arguments and creates a new GroupCommand object
+ */
 public class GroupCommandParser implements Parser<GroupCommand> {
 
-
+    /**
+     * Parses tje given {@code String} of arguments in the context of the GroupCommand
+     * and returns a GroupCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format.
+     */
     public GroupCommand parse(String args) throws ParseException {
         requireNonNull(args);
         String[] trimmedArgs = args.trim().split("\\s");
