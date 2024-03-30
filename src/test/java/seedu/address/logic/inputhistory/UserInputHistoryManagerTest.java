38a713182a292b27a212c9db438baa0b7f7be990
bachletuankhai <bachletuankhai@gmail.com>
2024-03-31 06:27:15 +0800

    Fix style issues

--- src/test/java/seedu/address/logic/inputhistory/UserInputHistoryManagerTest.java
+++ src/test/java/seedu/address/logic/inputhistory/UserInputHistoryManagerTest.java
@@ -1,12 +1,12 @@
 package seedu.address.logic.inputhistory;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class UserInputHistoryManagerTest {
 
