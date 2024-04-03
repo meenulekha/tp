60e5454624aa358f23313838a33b0b7592925152
julia-cp <juliac.purba@gmail.com>
2024-04-03 14:59:40 +0800

    fix test

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -200,6 +200,11 @@ public class AddCommandTest {
         }
 
         @Override
+        public void groupPerson(Person target, int groupNumber) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public ObservableList<Person> getFilteredPersonList() {
             throw new AssertionError("This method should not be called.");
         }
