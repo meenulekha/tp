99ffbe60fc6ea1b96d537fb2a3ccf8ab04e367c8
bachletuankhai <bachletuankhai@gmail.com>
2024-04-04 01:39:23 +0800

    Resolve merging conflict from PR #55

--- src/test/java/seedu/address/logic/parser/alias/FindAliasTest.java
+++ src/test/java/seedu/address/logic/parser/alias/FindAliasTest.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.FindCommand;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.ContactContainsKeywordsPredicate;
 
 class FindAliasTest {
     private FindAlias parser = new FindAlias();
@@ -23,7 +23,7 @@ class FindAliasTest {
     public void parse_validArgs_returnsFindCommand() {
         // no leading and trailing whitespaces
         FindCommand expectedFindCommand = new FindCommand(
-                new NameContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")));
+                new ContactContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")));
         assertParseSuccess(parser, "Alice Bob", expectedFindCommand);
 
         // multiple whitespaces between keywords
