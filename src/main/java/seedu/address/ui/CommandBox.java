c75a9c7ae5916aa438b482030d4a95b357867846
meenulekha <pmeenulekha@gmail.com>
2024-04-04 22:12:53 +0800

    no message

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -1,5 +1,8 @@
 package seedu.address.ui;
 
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
@@ -8,9 +11,6 @@ import javafx.scene.layout.Region;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
-
-import java.util.function.Consumer;
-import java.util.function.Supplier;
 
 /**
  * The UI component that is responsible for receiving user command inputs.
