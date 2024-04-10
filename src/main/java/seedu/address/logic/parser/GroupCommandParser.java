1b3c891fa5ffdea8cf31fd74a91f51b2222f5dcb
julia-cp <juliac.purba@gmail.com>
2024-04-11 00:06:11 +0800

    fix error messages

--- src/main/java/seedu/address/logic/parser/GroupCommandParser.java
+++ src/main/java/seedu/address/logic/parser/GroupCommandParser.java
@@ -6,11 +6,15 @@ import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.GroupCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Group;
 
 /**
  * Parses input arguments and creates a new GroupCommand object
  */
 public class GroupCommandParser implements Parser<GroupCommand> {
+
+    public static final String MESSAGE_NO_EXISTING_GROUP =
+            "Cannot group without a group number when there is no existing group";
 
     /**
      * Parses tje given {@code String} of arguments in the context of the GroupCommand
@@ -35,6 +39,9 @@ public class GroupCommandParser implements Parser<GroupCommand> {
         }
 
         if (trimmedArgs.length < 2) {
+            if (Group.getTotalGroupNumber() <= 0) {
+                throw new ParseException(MESSAGE_NO_EXISTING_GROUP);
+            }
             return new GroupCommand(index);
         } else {
             try {
