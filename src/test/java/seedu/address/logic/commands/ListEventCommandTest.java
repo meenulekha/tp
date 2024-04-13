f2084f2355e8983773927b1c7faa2b2c4170a441
meenulekha <pmeenulekha@gmail.com>
2024-04-13 16:52:03 +0800

    Add tests for ListEvent Command

--- src/test/java/seedu/address/logic/commands/ListEventCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListEventCommandTest.java
@@ -1,38 +1,45 @@
-//package seedu.address.logic.commands;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import seedu.address.model.Model;
-//import seedu.address.model.ModelManager;
-//import seedu.address.model.UserPrefs;
-//
-//import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-//import static seedu.address.testutil.TypicalEvents.getTypicalEventBook;
-//
-///**
-// * Contains integration tests (interaction with the Model) and unit tests for ListEventCommand.
-// */
-//public class ListEventCommandTest {
-// ;
-////    private Model model;
-////    private Model expectedModel;
-////
-////    @BeforeEach
-////    public void setUp() {
-////        model = new ModelManager(getTypicalEventBook(), new UserPrefs());
-////        expectedModel = new ModelManager(model.getEventBook(), new UserPrefs());
-////    }
-////
-////    @Test
-////    public void execute_listIsNotFiltered_showsSameList() {
-////        assertCommandSuccess(new ListEventCommand(), model, ListEventCommand.MESSAGE_SUCCESS, expectedModel);
-////    }
-////
-////    @Test
-////    public void execute_listIsFiltered_showsEverything() {
-////        // No need to show any specific event, as we're testing the behavior when the list is filtered
-////        assertCommandSuccess(new ListEventCommand(), model, ListEventCommand.MESSAGE_SUCCESS, expectedModel);
-////    }
-////}
-//
-//
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.logic.commands.ListEventCommand.MESSAGE_SUCCESS;
+import static seedu.address.testutil.TypicalEvents.getTypicalEventBook;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for ListEventCommand.
+ */
+public class ListEventCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), getTypicalEventBook(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), model.getEventBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_listIsNotFiltered_showsSameList() {
+        ListEventCommand listEventCommand = new ListEventCommand();
+        assertEquals(listEventCommand.execute(model).getFeedbackToUser(), MESSAGE_SUCCESS);
+    }
+
+    @Test
+    public void execute_listIsFiltered_showsEverything() {
+        // No need to show any specific event, as we're testing the behavior when the list is filtered
+        ListEventCommand listEventCommand = new ListEventCommand();
+        assertEquals(listEventCommand.execute(model).getFeedbackToUser(), MESSAGE_SUCCESS);
+    }
+}
+
+
+
+
